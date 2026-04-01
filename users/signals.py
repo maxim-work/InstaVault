ec7d9a846b45916559362f9c304f896b4f519558
maxim-work <maxim.official.work1@gmail.com>
2026-04-01 20:04:45 +0300

    fix(users): wrap user settings creation in transaction.on_commit

--- users/signals.py
+++ users/signals.py
@@ -1,8 +1,12 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from .models import CustomUser, UserSettings
+from django.db import transaction
+
 
 @receiver(post_save, sender=CustomUser)
 def create_user_settings(sender, instance, created, **kwargs):
     if created:
-        UserSettings.objects.create(user=instance)
+        transaction.on_commit(
+            lambda: UserSettings.objects.create(user=instance)
+        )
