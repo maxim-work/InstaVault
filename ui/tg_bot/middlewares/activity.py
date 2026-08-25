e0db14e07f3cc2f36a8bf59b681ae2800b5e0b3a
maxim-work <maxim.official.work1@gmail.com>
2026-08-25 19:43:26 +0300

    perf: cache activity updates with interval

--- ui/tg_bot/middlewares/activity.py
+++ ui/tg_bot/middlewares/activity.py
@@ -1,8 +1,9 @@
-from aiogram import BaseMiddleware
-from aiogram.types import CallbackQuery, Message, Update, TelegramObject
-from collections.abc import Awaitable, Callable
-from typing import Any
+import time
 
+from aiogram import BaseMiddleware
+from aiogram.types import CallbackQuery, Message, Update
+
+from config import ACTIVITY_UPDATE_INTERVAL_SECONDS
 from data.db.users import UserDB
 from ui.tg_bot.middlewares.utils import get_real_event
 
@@ -10,20 +11,25 @@ from ui.tg_bot.middlewares.utils import get_real_event
 class ActivityMiddleware(BaseMiddleware):
     def __init__(self, user_db: UserDB) -> None:
         self.user_db = user_db
+        self.last_flush: dict[int, float] = {}
         super().__init__()
 
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: TelegramObject,
-        data: dict[str, Any],
-    ) -> Any:
-        if not isinstance(event, Update):
-            return await handler(event, data)
+    async def __call__(self, handler, event, data):
+        if isinstance(event, Update):
+            real_event = get_real_event(event)
 
-        real_event = get_real_event(event)
+            if (
+                isinstance(real_event, (Message, CallbackQuery))
+                and real_event.from_user
+            ):
+                tg_id = real_event.from_user.id
+                now = time.monotonic()
 
-        if isinstance(real_event, (Message, CallbackQuery)) and real_event.from_user:
-            self.user_db.update_last_active(real_event.from_user.id)
+                if (
+                    tg_id not in self.last_flush
+                    or now - self.last_flush[tg_id] > ACTIVITY_UPDATE_INTERVAL_SECONDS
+                ):
+                    self.user_db.update_last_active(tg_id)
+                    self.last_flush[tg_id] = now
 
         return await handler(event, data)
